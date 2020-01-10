adf65f65348c173414696c1ef60d0bb9b1a7713a
Roshan Bista <bistaroshan03@gmail.com>
2020-01-10 14:56:52 +0545

    Added automigration for debug flag

--- db/db.go
+++ db/db.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/jinzhu/gorm"
 
+	"github.com/zeek-r/weather-monster/models"
+
 	// Import driver
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
@@ -13,5 +15,12 @@ func Init() (*gorm.DB, error) {
 	config := LoadConfig()
 	connString := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable", config.dbHost, config.dbPort, config.dbUser, config.dbName, config.dbPass)
 	db, err := gorm.Open(config.dbType, connString)
+
+	if config.debug == true {
+		// create tables in debug mode only, not recommened for prod
+		db.AutoMigrate(&models.City{})
+		db.AutoMigrate(&models.Temperature{})
+		db.AutoMigrate(&models.Webhook{})
+	}
 	return db, err
 }
