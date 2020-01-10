5abd7eaad6e151d81c34b09cb95029be93809640
Roshan Bista <bistaroshan03@gmail.com>
2020-01-10 14:07:39 +0545

    Added error handling for create, update, delete

--- db/cityRepo/repo.go
+++ db/cityRepo/repo.go
@@ -15,19 +15,33 @@ func Init(Conn *gorm.DB) Repository {
 }
 
 func (repo *cityRepository) Create(toInsert *models.City) error {
-
+	created := repo.Conn.Create(toInsert)
+	if created.Error != nil {
+		return created.Error
+	}
 	return nil
 }
 
 func (repo *cityRepository) Update(toUpdate *models.City) error {
+	updated := repo.Conn.Model(&models.City{}).Updates(toUpdate)
 
+	if updated.Error != nil {
+		return updated.Error
+	}
 	return nil
 }
 
-func (repo *cityRepository) DeleteByID(id int64) error {
+func (repo *cityRepository) DeleteByID(toDelete *models.City) error {
+	deleted := repo.Conn.Model(&models.City{}).Delete(toDelete)
+
+	if deleted.Error != nil {
+		return deleted.Error
+	}
 	return nil
 }
 
-func (repo *cityRepository) GetByID(id int64) (*models.City, error) {
-	return nil, nil
+func (repo *cityRepository) GetByID(id int64) (models.City, error) {
+	var city models.City
+	repo.Conn.First(&city, id)
+	return city, nil
 }
