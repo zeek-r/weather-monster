575e73b7b9936f5fcbf1b61476446c96cf0bf11c
Roshan Bista <bistaroshan03@gmail.com>
2020-01-10 11:24:51 +0545

    Added service initializer

---
+++ app/services/service.go
@@ -0,0 +1,48 @@
+package service
+
+import (
+	"github.com/jinzhu/gorm"
+
+	cityservice "github.com/zeek-r/weather-monster/app/services/city"
+	cityrepo "github.com/zeek-r/weather-monster/db/cityRepo"
+
+	forecastservice "github.com/zeek-r/weather-monster/app/services/forecast"
+
+	temperatureservice "github.com/zeek-r/weather-monster/app/services/temperature"
+	temperaturerepo "github.com/zeek-r/weather-monster/db/temperatureRepo"
+
+	webhookservice "github.com/zeek-r/weather-monster/app/services/webhook"
+	webhookrepo "github.com/zeek-r/weather-monster/db/webhookRepo"
+)
+
+type Service struct {
+	CityService        *cityservice.Service
+	ForecastService    *forecastservice.Service
+	TemperatureService *temperatureservice.Service
+	WebhookService     *webhookservice.Service
+}
+
+func Init(dbConn *gorm.DB) *Service {
+
+	/** Repo and services initialization start **/
+
+	cityRepo := cityrepo.Init(dbConn)
+	cityService := cityservice.NewCityService(cityRepo)
+
+	temperatureRepo := temperaturerepo.Init(dbConn)
+	temperatureService := temperatureservice.NewTemperatureService(temperatureRepo)
+
+	forecastService := forecastservice.NewForecastService(temperatureRepo)
+
+	webhookRepo := webhookrepo.Init(dbConn)
+	webhookService := webhookservice.NewWebhookService(webhookRepo)
+
+	/** Repo and services initialization end **/
+
+	return &Service{
+		CityService:        &cityService,
+		ForecastService:    &forecastService,
+		TemperatureService: &temperatureService,
+		WebhookService:     &webhookService,
+	}
+}
