85c1f72b25d8c02f7f223c8b6ae70395a8400a86
Roshan Bista <bistaroshan03@gmail.com>
2020-01-10 11:21:03 +0545

    Added forecast router

---
+++ api/http/routes/forecast.go
@@ -0,0 +1,10 @@
+package routes
+
+import (
+	"github.com/gorilla/mux"
+	handler "github.com/zeek-r/weather-monster/api/http/handlers/forecast"
+)
+
+func LoadForecastRoutes(router *mux.Router, forecastHandler *handler.ForecastHandler) {
+	router.HandleFunc("/forecasts/{city_id}", forecastHandler.Create).Methods("GET")
+}
