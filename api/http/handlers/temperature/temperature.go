84986b7d0aea00a43c6d5f968a1dc51d3ff227e5
Roshan Bista <bistaroshan03@gmail.com>
2020-01-10 14:42:17 +0545

    Implemented initial features temperature handler

--- api/http/handlers/temperature/temperature.go
+++ api/http/handlers/temperature/temperature.go
@@ -1,31 +1,54 @@
 package temperaturehandler
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 
+	"github.com/zeek-r/weather-monster/api/http/utils/response"
 	temperature "github.com/zeek-r/weather-monster/app/services/temperature"
+	"github.com/zeek-r/weather-monster/models"
+	"github.com/zeek-r/weather-monster/pkg/logger"
 )
 
 type TemperatureHandler struct {
-	temperatureService *temperature.Service
+	temperatureService temperature.Service
 }
 
-func NewTemperatureHandler(service *temperature.Service) *TemperatureHandler {
+func NewTemperatureHandler(service temperature.Service) *TemperatureHandler {
 	return &TemperatureHandler{service}
 }
 
-func (temperatureService *TemperatureHandler) Create(w http.ResponseWriter, r *http.Request) {
+func (handler *TemperatureHandler) Create(w http.ResponseWriter, r *http.Request) {
+	var request models.Temperature
 
-}
+	defer r.Body.Close()
 
-func (temperatureService *TemperatureHandler) Update(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
 
-}
+	if err != nil {
+		message := "Error reading body"
+		logger.Error(err, message)
+		response.Err(w, message, 500)
+		return
+	}
 
-func (temperatureService *TemperatureHandler) DeleteByID(w http.ResponseWriter, r *http.Request) {
+	if err := json.Unmarshal(body, &request); err != nil {
+		message := "Error unmarshalling city"
+		logger.Error(err, message)
+		response.Err(w, message, 500)
+		return
+	}
 
-}
+	logger.Info("Creating Temperature record", map[string]interface{}{"cityId": request.CityID})
 
-func (temperatureService *TemperatureHandler) GetByID(w http.ResponseWriter, r *http.Request) {
+	if err := handler.temperatureService.Create(&request); err != nil {
+		message := "Error Creating temperature record"
+		logger.Error(err, message)
+		response.Err(w, message, 400)
+		return
+	}
 
+	response.Json(w, map[string]interface{}{"success": true, "message": "Temperature created successfully"})
+	return
 }
