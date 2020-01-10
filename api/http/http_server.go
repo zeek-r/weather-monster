5882286c7d9fc39872a818ba63911a8c213f1852
Roshan Bista <bistaroshan03@gmail.com>
2020-01-10 11:18:00 +0545

    Added http server

---
+++ api/http/http_server.go
@@ -0,0 +1,56 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/handlers"
+	logger "github.com/sirupsen/logrus"
+	handler "github.com/zeek-r/weather-monster/api/http/handlers"
+	router "github.com/zeek-r/weather-monster/api/http/routes"
+	application "github.com/zeek-r/weather-monster/app"
+)
+
+type API struct {
+	app *application.App
+}
+
+func NewAPI(app *application.App) *API {
+	return &API{app}
+}
+
+func (api *API) Serve() error {
+
+	// Load Config
+	config, err := LoadConfig()
+	if err != nil {
+		logger.Error("Error loading config", err)
+		return err
+	}
+
+	// Handler CORS
+	cors := handlers.CORS(
+		handlers.AllowedOrigins([]string{"*"}),
+		handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "OPTIONS", "DELETE"}),
+		handlers.AllowedHeaders([]string{"Content-Type", "Authorization", "X-ENVAPI-USERNAME"}),
+	)
+
+	// Load Handlers
+	Handlers := handler.Init(api.app)
+
+	routes := router.LoadRoutes("/api", Handlers)
+
+	server := &http.Server{
+		Addr:        fmt.Sprintf(":%d", config.Port),
+		Handler:     cors(routes),
+		ReadTimeout: 2 * time.Minute,
+	}
+	logger.Infof("serving api at %d", config.Port)
+
+	if err := server.ListenAndServe(); err != http.ErrServerClosed {
+		logger.Error(err)
+		return err
+	}
+	return nil
+}
