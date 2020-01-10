1fdec0131399573bcd880c7e51dc075aec77f20f
Roshan Bista <bistaroshan03@gmail.com>
2020-01-10 11:25:07 +0545

    Added city interface

---
+++ app/services/city/interface.go
@@ -0,0 +1,12 @@
+package cityservice
+
+import (
+	"github.com/zeek-r/weather-monster/models"
+)
+
+type Service interface {
+	Create(toInsert *models.City) error
+	Update(toUpdate *models.City) error
+	DeleteByID(id int64) error
+	GetByID(id int64) (*models.City, error)
+}
