5e4476095b6417c97b4e5280d120e80ce9af695f
Roshan Bista <bistaroshan03@gmail.com>
2020-01-10 14:06:57 +0545

    Changed delete function signature

--- db/cityRepo/interface.go
+++ db/cityRepo/interface.go
@@ -7,6 +7,5 @@ import (
 type Repository interface {
 	Create(toInsert *models.City) error
 	Update(toUpdate *models.City) error
-	DeleteByID(id int64) error
-	GetByID(id int64) (*models.City, error)
+	DeleteByID(toDelete *models.City) error
 }
